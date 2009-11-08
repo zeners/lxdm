df6fa348e99e58493c2829e053a068b0e69692d9
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2009-11-08 10:27:20 +0000

    Little fix for setting locale.

--- src/lxdm.c
+++ src/lxdm.c
@@ -682,7 +682,11 @@ void lxdm_do_login(struct passwd *pw,char *session,char *lang)
 		env[i++]=g_strdup_printf("PATH=%s",path);
 		g_free(path);
 		if(lang && lang[0])
+        {
 			env[i++]=g_strdup_printf("LANG=%s",lang);
+			env[i++]=g_strdup_printf("LANGUAGE=%s",lang);
+			env[i++]=g_strdup_printf("LC_ALL=%s",lang);
+        }
 		if(getenv("XDG_SESSION_COOKIE"))
 			env[i++]=g_strdup_printf("XDG_SESSION_COOKIE=%s",getenv("XDG_SESSION_COOKIE"));
 		env[i++]=0;
